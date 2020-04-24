5aba198247a2c8296195c4c45141759898877249
Delberin Ali <delberin.ali@powercoders.org>
2020-04-24 15:40:56 +0100

    now i finish from the exercise 6

---
+++ exercise6/script.js
@@ -0,0 +1,14 @@
+function weekNumberstoWords () {
+    var weekday = new Array(7);
+    weekday[1] = "Sunday";
+    weekday[2] = "Monday";
+    weekday[3] = "Tuesday";
+    weekday[4] = "Wednesday";
+    weekday[5] = "Thursday";
+    weekday[6] = "Friday";
+    weekday[7] = "Saturday";
+  
+  
+   console.log(weekday[7] + " " + weekday[1] + " " + weekday[3] + " " + weekday[6]);
+  }
+  weekNumberstoWords ()
