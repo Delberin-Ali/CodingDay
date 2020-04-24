2927706bb01791eb57fcbe5cafc216be378a3478
Delberin Ali <delberin.ali@powercoders.org>
2020-04-24 15:59:18 +0100

    this is my last commit

--- exercise4/script.js
+++ exercise4/script.js
@@ -1,6 +1,18 @@
- var books={
+ let  books= [
+      {
+          firstBook: "Think like a freak",
+          firstPrice: "120 CHF"
+      },
+      {
+         firstBook: "5 second rules",
+         firstPrice: "100 CHF"
+      },
+      {
+         firstBook: "Peace",
+         firstPrice: "150 CHF"
+      }
+  ];
+console.log(books)
+
      
-    BooksName :"",
-    booksPrice:"",
-     
- };
+ 
